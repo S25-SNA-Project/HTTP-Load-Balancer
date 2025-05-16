f15349af482675501aaff90a904fac6b7ecd0de1
i.lobazov <i.lobazov@innopolis.university>
2025-05-16 06:15:27 +0300

    Fix issue with filesystem preparation

--- balancer_server/lib/config_reader.py
+++ balancer_server/lib/config_reader.py
@@ -1,10 +1,15 @@
 import json
+from pathlib import Path
 from lib.servers_heap import ServersHeap
 from logging import getLogger, INFO, StreamHandler, Formatter
 
 
 with open("config.json", "r") as file:
     config = json.load(file)
+
+for path_part in Path(config["log_file"]).parents:
+    if not path_part.exists():
+        path_part.mkdir(parents=True, exist_ok=True)
 
 chanel = StreamHandler(open(config["log_file"], "a"))
 chanel.setFormatter(Formatter(
